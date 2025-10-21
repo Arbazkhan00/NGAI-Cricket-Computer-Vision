fca564988337eb9c5d53b28dce5d5850eaf30f3b
arbaz khan <arbazjani8@gmail.com>
2025-10-21 21:13:47 +0500

    today done

--- NGAI-Cricekt/src/App.jsx
+++ NGAI-Cricekt/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
      <main class="page">
     <div class="type-area" id="typeArea">
-      jab code kro gay to jab sahi treeky sy localy run ho jay ga fr yaha hum as a website dehly gay sub
+      jab code kro gay to jab sahi treeky sy localy run ho jay ga <br />fr git py push kr diana hai or yaha hum as a website dehky gay sub
     </div>
   </main>
     </>
